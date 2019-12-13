0aaaadf17d02e351d4635187632a80553f6625dd
koressel <koressel.dev@gmail.com>
2019-12-13 10:45:28 -0600

    promisify router

--- controllers/productupload.js
+++ controllers/productupload.js
@@ -5,39 +5,17 @@ const products = require('../models/products');
 
 router.post('/', (req, res, next) => {
 
-  // let data = {};
-
-  // let body = req.body;
-  // data.title = body.title;
-  // data.dimensions = body.dimensions;
-  // data.price = body.price;
-  // data.shipping = body.shipping;
-  // let productImage = req.files.productImage.data;
-
   products.create(req)
+
     .then(result => {
       res.sendStatus(200);
     })
+    
     .catch(err => {
       console.log(err)
       res.sendStatus(400);
     });
 
-  // imgur.upload(productImage)
-  //   .then(url => {
-
-  //     data.image = url;
-
-  //     products.create(data)
-  //       .then(result => {
-  //         res.sendStatus(200);
-  //       })
-  //       .catch(err => res.sendStatus(400));
-  //   })
-  //   .catch(err => {
-  //     console.log(err);
-  //     res.sendStatus(400);
-  //   });
 });
 
 module.exports = router;
